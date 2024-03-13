7d0fb89c244762b07c9bfda291d5836b7f18175d
NorenaB <163215825+NorenaB@users.noreply.github.com>
2024-03-12 20:04:05 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Vilma";
+//nombre = "Lucia";
+
+
+//var nombre1 = "Vilma";
+
+//const nombre2 = "Vilma";
+
+//console.log(nombre)
+//console.log(nombre1)
+//console.log(nombre2)
+
+//SELECCIONAR ELEMENTOS
+
+let contenidoTitulo = "Nombre"
+
+let titulo = document.querySelector(*.logo .fuente-)
+titulo.innerHTML =  contenidoTitulo;
+
+//CONDICUONALES 
+let textoTitulo = titulo=innertext;
+console.log(textoTitulo);
+
+if(textoTitulo == "Nombre") {
+    titulo.innerHTML = "otro";
+} else {
+    console.log("no se cumple");
+}
